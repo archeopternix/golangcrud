03a9d6e15f04a2eedf8b138ebab80f0ecd36f8e8
archeopternix <39010055+archeopternix@users.noreply.github.com>
2021-03-16 21:11:51 +0100

    HTML view refactored

--- main.go
+++ main.go
@@ -15,11 +15,15 @@ func main() {
 	//	fmt.Println(a.StringYAML())
 
 	c := NewGenerator()
-	if err := c.AddModule("modules/model/models.yaml"); err != nil {
-		log.Fatalf("ERROR: %v", err)
-	}
+	/*	if err := c.AddModule("modules/model/models.yaml"); err != nil {
+			log.Fatalf("ERROR: %v", err)
+		}
 
-	if err := c.AddModule("modules/database/database.yaml"); err != nil {
+		if err := c.AddModule("modules/database/database.yaml"); err != nil {
+			log.Fatalf("ERROR: %v", err)
+		}
+	*/
+	if err := c.AddModule("modules/view/view.yaml"); err != nil {
 		log.Fatalf("ERROR: %v", err)
 	}
 
